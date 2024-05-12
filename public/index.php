c80982f6254ff954cc9be19e948a7e4b9111777a
germondai <bruno.krocek@gmail.com>
2024-05-12 18:36:55 +0200

    updated public/index.php

--- public/index.php
+++ public/index.php
@@ -9,25 +9,30 @@ include $basePath . "src/includes/header.php";
 ?>
 
 <main class="w-screen h-screen grid place-items-center">
-    <div class="flex flex-col items-center gap-4">
+    <div class="flex flex-col items-center gap-6">
         <div class="flex items-center gap-4">
-            <img src="assets/img/favicon.ico" alt="Rocket" class="size-8">
-            <h1 class="font-bold">PHP Starter</h1>
+            <a href="https://github.com/germondai/php-starter/" target="_blank" class="flex items-center gap-4 [font-size:var(--font-h1)] font-bold underline">
+                <span>PHP Starter</span>
+                <img src="assets/img/favicon.ico" alt="Rocket" class="size-8">
+            </a>
         </div>
-        <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center gap-2">
             <b>Features</b>
             <ul class="list-disc">
-                <li>Security features</li>
-                <li>Own API system</li>
-                <li>Tailwind CSS</li>
-                <li>jQuery</li>
+                <li>Own API System</li>
+                <li>Security</li>
+                <ul class="list-disc pl-5">
+                    <li>Routing</li>
+                    <li>File and Dir access</li>
+                </ul>
+                <li>Custom Utils</li>
+                <li>Organized Structure</li>
                 <li>Nette</li>
                 <ul class="list-disc pl-5">
-                    <li>DB Explorer</li>
+                    <li>Database Explorer</li>
                     <li>Tracy</li>
                 </ul>
                 <li>Environment (.env)</li>
-                <li>Clean Interface 🤩</li>
             </ul>
         </div>
     </div>
