1a68ddbbbaa46350be0150a974adab7805caad2a
germondai <bruno.krocek@gmail.com>
2024-06-03 16:08:16 +0200

    base of doctrine migrations

--- api/index.php
+++ api/index.php
@@ -4,15 +4,16 @@ declare(strict_types=1);
 
 # imports
 use Api\ApiController;
+use Api\Entity\Article;
+use Api\Entity\User;
 
 # require config
 require_once "../src/includes/config.php";
 
-# set json and cors headers
+# set cors headers
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE');
 header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With");
-header('Content-Type: application/json; charset=utf-8');
 
 # preflight error fix
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
@@ -20,6 +21,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     die();
 }
 
-# handle api request
-$api = new ApiController();
-$api->run();
+$orm = true;
+if ($orm) {
+    $user = (new User())
+        ->setName('Jméno')
+        ->setSurname('Příjmení')
+        ->setEmail('jmeno@prijmeni.cz')
+        ->setPassword(password_hash('heslo', PASSWORD_BCRYPT))
+        ->setCreatedAt(new DateTime());
+
+    dump($user);
+
+    $articles = [
+        ['Název', 'Obsah'],
+        ['Název2', 'Obsah2']
+    ];
+
+    foreach ($articles as [$title, $content]) {
+        $article = (new Article())
+            ->setTitle($title)
+            ->setContent($content);
+
+        $user->addArticle($article);
+    }
+
+    dump($user);
+} else {
+    # set json header
+    header('Content-Type: application/json; charset=utf-8');
+
+    # handle api request
+    $api = new ApiController();
+    $api->run();
+}
