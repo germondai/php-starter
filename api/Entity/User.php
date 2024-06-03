4008f71927231cfc317b05545a911807e43fbbef
germondai <bruno.krocek@gmail.com>
2024-06-03 12:33:02 +0200

    new user entity

--- api/Entity/User.php
+++ api/Entity/User.php
@@ -5,7 +5,61 @@ declare(strict_types=1);
 namespace Api\Entity;
 
 use Api\BaseEntity;
+use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\Entity]
+#[ORM\Table(name: 'users')]
 class User extends BaseEntity
 {
+    #[ORM\Column(type: 'string', length: 256)]
+    private string $name;
+
+    #[ORM\Column(type: 'string', length: 256)]
+    private string $surname;
+
+    #[ORM\Column(type: 'string', length: 256, unique: true)]
+    private string $email;
+
+    #[ORM\Column(type: 'string', length: 256)]
+    private string $password;
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getSurname(): string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): void
+    {
+        $this->surname = $surname;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
 }
