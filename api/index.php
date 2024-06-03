a3068474bde33d3548e73ea2eb43c06b0ae51034
germondai <bruno.krocek@gmail.com>
2024-06-03 16:50:31 +0200

    api index back to normal

--- api/index.php
+++ api/index.php
@@ -4,16 +4,15 @@ declare(strict_types=1);
 
 # imports
 use Api\ApiController;
-use Api\Entity\Article;
-use Api\Entity\User;
 
 # require config
 require_once "../src/includes/config.php";
 
-# set cors headers
+# set json and cors headers
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE');
 header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With");
+header('Content-Type: application/json; charset=utf-8');
 
 # preflight error fix
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
@@ -21,41 +20,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     die();
 }
 
-$orm = true;
-if ($orm) {
-    $user = (new User())
-        ->setName('Jméno')
-        ->setSurname('Příjmení')
-        ->setEmail('jmeno@prijmeni.cz')
-        ->setPassword(password_hash('heslo', PASSWORD_BCRYPT))
-        ->setCreatedAt(new DateTime());
-
-    dump($user);
-
-    $articles = [
-        ['Název', 'Obsah'],
-        ['Název2', 'Obsah2']
-    ];
-
-    foreach ($articles as [$title, $content]) {
-        $article = (new Article())
-            ->setTitle($title)
-            ->setContent($content)
-            ->setCreatedAt(new DateTime());
-
-        $user->addArticle($article);
-        $entityManager->persist($article);
-    }
-
-    $entityManager->persist($user);
-    $entityManager->flush();
-
-    dump($user);
-} else {
-    # set json header
-    header('Content-Type: application/json; charset=utf-8');
-
-    # handle api request
-    $api = new ApiController();
-    $api->run();
-}
+# handle api request
+$api = new ApiController();
+$api->run();
